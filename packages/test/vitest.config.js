09cd6a564637834f81cf198aadf8bf956b00398e
Bill Collins <bill.collins@hp.com>
2025-01-23 14:23:07 +0000

    More tests

--- packages/test/vitest.config.js
+++ packages/test/vitest.config.js
@@ -1,3 +1,11 @@
 import {defineConfig} from 'vitest/config';
 
-export default defineConfig({})
+
+export default defineConfig({
+  test: {
+    include: ['tests/*'],
+    coverage: {
+      provider: 'istanbul',
+    }
+  }
+});
