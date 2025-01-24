15eacfe775449f0c6e531efbebb9a9ba5f44e69c
Bill Collins <bill.collins@hp.com>
2025-01-24 09:23:32 +0000

    Browser breaks it!

--- packages/test/vitest.config.js
+++ packages/test/vitest.config.js
@@ -3,6 +3,14 @@ import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
   test: {
+    browser: {
+      enabled: true, instances: [
+        {
+          browser: "chromium",
+        },
+      ],
+      provider: "playwright",
+    },
     include: ['tests/*'],
     coverage: {
       provider: 'istanbul',
--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,14 @@ import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
   test: {
+    browser: {
+      enabled: true, instances: [
+        {
+          browser: "chromium",
+        },
+      ],
+      provider: "playwright",
+    },
     include: ['tests/*'],
     coverage: {
       provider: 'istanbul',
